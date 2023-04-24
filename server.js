42d18000e25e0ad3bb11c2e60cfac69af12b4798
Udaykiran <koradaudaykiran1406@gmail.com>
2023-04-24 23:04:34 +0530

    deployment started1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mclient=require("mongodb").MongoClient;
 require("dotenv").config;
 // integrating front and back
 const path=require('path');
-app.use(express.static(path.join(__dirname,"build")))
+app.use(express.static(path.join(__dirname,"./build")))
 // db connection url
 const DBurl= 'mongodb+srv://mydatabase:mydatabase@mycluster.3qnvw.mongodb.net/?retryWrites=true&w=majority';
 
